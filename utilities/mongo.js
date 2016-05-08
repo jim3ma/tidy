82235999fd3dde45581d5917f817dc2c4c32f60d
Jim Mar <majinjing3@gmail.com>
2016-05-08 03:14:39 -0700

    update mongo.js

--- utilities/mongo.js
+++ utilities/mongo.js
@@ -3,26 +3,26 @@ db.createRole(
    {
      role: "tidyOpRole",
      privileges: [
-       { 
-           resource: { db: "tidy", collection: "" }, 
-           actions: [ 
-               "collStats", 
-               "convertToCapped", 
-               "createCollection", 
-               "dbHash", 
-               "dbStats", 
-               "dropCollection", 
-               "createIndex", 
-               "dropIndex", 
-               "emptycapped", 
-               "find", 
-               "insert", 
-               "killCursors", 
-               "listIndexes", 
-               "listCollections", 
-               "remove", 
-               "renameCollectionSameDB", 
-               "update" ] 
+       {
+           resource: { db: "tidy", collection: "" },
+           actions: [
+               "collStats",
+               "convertToCapped",
+               "createCollection",
+               "dbHash",
+               "dbStats",
+               "dropCollection",
+               "createIndex",
+               "dropIndex",
+               "emptycapped",
+               "find",
+               "insert",
+               "killCursors",
+               "listIndexes",
+               "listCollections",
+               "remove",
+               "renameCollectionSameDB",
+               "update" ]
        }
      ],
      roles: []
@@ -30,11 +30,17 @@ db.createRole(
 );
 
 db.createUser(
-    { 
-        user:"tidy", 
-        pwd:"111111", 
+    {
+        user:"tidy",
+        pwd:"111111",
         roles:[
             "tidyOpRole"
         ]
     }
 );
+
+// create Index
+
+db.user.ensureIndex({"email": 1});
+
+db.checkin.ensureIndex({"timestamp": 1});
