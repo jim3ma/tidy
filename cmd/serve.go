836d14573449ad13929db1c41c74d0f9990a5c11
Jim Mar <majinjing3@gmail.com>
2016-05-10 22:34:41 +0800

    update command comment

--- cmd/serve.go
+++ cmd/serve.go
@@ -32,7 +32,7 @@ import (
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
+	Short: "Start tidy",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
